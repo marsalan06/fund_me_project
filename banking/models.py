bccb691ef821abf1c9b81efaca8be1f8fc3d5089
Ramish Qasim <ramishqasim@gmail.com>
2023-06-06 17:21:37 +0500

    [Modified] structure of product table

--- banking/models.py
+++ banking/models.py
@@ -18,17 +18,20 @@ class Product(models.Model):
     name = models.CharField(max_length=100, default=None, blank=True, null=True)
     bank = models.ForeignKey(Bank, on_delete=models.CASCADE)
     minimum_balance = models.DecimalField(max_digits=10, decimal_places=2)
-    indicative_rate = models.DecimalField(max_digits=5, decimal_places=2)
+    rate_lower_limit = models.DecimalField(max_digits=5, decimal_places=2)
+    rate_upper_limit = models.DecimalField(max_digits=5, decimal_places=2)
     profit_payout_freq_choices = (
         ('D', 'Daily'),
         ('M', 'Monthly'),
         ('Q', 'Quarterly'),
         ('BA', 'Biannually'),
         ('A', 'Annually'),
+        ('MA', 'Maturity'),
     )
     profit_payout_freq = models.CharField(max_length=2, choices=profit_payout_freq_choices)
-    earning_per_1000 = models.DecimalField(max_digits=10, decimal_places=2)
-    premature_fee = models.DecimalField(max_digits=10, decimal_places=2)
+    earning_per_1000_lower_limit = models.DecimalField(max_digits=10, decimal_places=2)
+    earning_per_1000_upper_limit = models.DecimalField(max_digits=10, decimal_places=2)
+    premature_encashment = models.CharField(max_length=100)
 
     def __str__(self):
         return f"Product {self.id} - Bank: {self.bank.name}"
