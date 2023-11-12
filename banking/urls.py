413f1c108587c265793a1c2b65b60519c423291a
marsalan06 <arsalan.9798@gmail.com>
2023-11-12 17:22:11 +0500

    islamic,conventional, term deposite, faq added

--- banking/urls.py
+++ banking/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('mutualfunds/', views.mutual_funds, name='mutualfunds'),
-    path('calculator/', views.calculator, name='calculator')
+    path('calculator/', views.calculator, name='calculator'),
+    path('islamic/', views.islamic, name='islamic'),
+    path('conventional/', views.conventional, name='conventional'),
+    path('term-deposite/', views.term_deposites, name='term_deposites'),
+    path('faq/', views.faqs, name='faqs')
+
 
     # Add more URL patterns here if needed
 ]
--- banking/views.py
+++ banking/views.py
@@ -3,13 +3,29 @@ from django.shortcuts import render
 # Create your views here.
 
 
-from django.shortcuts import render
-
 def home(request):
     return render(request, 'index.html')
-    
+
+
 def mutual_funds(request):
     return render(request, 'mutual_funds.html')
 
+
 def calculator(request):
     return render(request, 'calculator.html')
+
+
+def islamic(request):
+    return render(request, 'islamic.html')
+
+
+def conventional(request):
+    return render(request, 'conventional.html')
+
+
+def term_deposites(request):
+    return render(request, 'term_deposite.html')
+
+
+def faqs(request):
+    return render(request, 'faqs.html')
