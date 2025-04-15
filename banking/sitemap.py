dc2e008f565d336b38e39ad2cbbca5e74b42dbf7
mramishqasim <ramishqasim@gmail.com>
2025-04-15 21:08:28 +0500

    updated sitemap

--- banking/sitemap.py
+++ banking/sitemap.py
@@ -1,12 +1,18 @@
+# banking/sitemap.py
 from django.contrib.sitemaps import Sitemap
-from django.shortcuts import reverse
+from django.urls import reverse
 
 class StaticViewSitemap(Sitemap):
-    priority = 0.8
     changefreq = 'monthly'
+    priority = 0.8
 
     def items(self):
-        return ['index', 'mutual_funds', 'term_deposit', 'life_insurance']
+        return [
+            'home', 'mutualfunds', 'calculator', 'islamic', 'conventional', 'term_deposites', 'faqs',
+            'investment_list', 'foreign_currency', 'life_insurance', 'life_insurance_companies', 
+            'mutual_fund_companies', 'life_insurance_list', 'future_value_calculator', 'calculators',
+            'quarterly', 'monthly', 'biannual', 'recurring', 'ruleof72', 'ruleof78', 'mutualfundcalculator', 'upload_article'
+        ]
 
     def location(self, item):
         return reverse(item)
